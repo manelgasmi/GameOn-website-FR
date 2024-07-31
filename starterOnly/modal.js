476e8cb06dbc658c1dc9d9a2227e59fe146b0e97
Manel Gasmi <manel.gasmi02@gmail.com>
2024-07-31 12:50:31 +0200

    issue #2: finalisation de la validation du formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -27,5 +27,59 @@ btnClose.addEventListener("click", closeModal);
 // Close modal form
 function closeModal() {
   modalbg.style.display = "none";
+}
 
+// form validation
+function validate(event) {
+  event.preventDefault();
+
+  // First name validation
+  const firstNameInput = document.getElementById("first");
+  const firstName = firstNameInput.value;
+  if (firstName.length < 2) {
+    console.log("Erreur prénom doit être minimum 2 lettres");
+  }
+
+  // Last name validation
+  const lastNameInput = document.getElementById("last");
+  const lastName = lastNameInput.value;
+  if (lastName.length < 2) {
+    console.log("Erreur nom doit être minimum 2 lettres");
+  }
+
+  // email validation
+  const emailInput = document.getElementById("email");
+  let email = emailInput.value;
+  let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+.[a-z0-9._-]+");
+  if (emailRegExp.test(email)) {
+    console.log("Email valide");
+  } else {
+    console.log("Erreur email non valide");
+  }
+
+  // number of Competition validation
+  const nbrCompetitions = document.getElementById("quantity");
+  const nbr = nbrCompetitions.value;
+  if (nbr < 0 || nbr > 99 || !nbr) {
+    console.log("Erreur nombre de compétition doit être compris entre 0 et 99");
+  }
+
+  // input radio validation
+  let radioInputs = document.querySelectorAll('input[name = "location"]');
+  let locationChecked = false;
+  for (let i = 0; i < radioInputs.length; i++) {
+    if (radioInputs[i].checked) {
+      locationChecked = true;
+      break;
+    }
+  }
+  if(locationChecked === false) {
+    console.log("Erreur vous devez choisir une localisation");
+  }
+
+  // general conditions box validation
+  const generalConditions = document.getElementById("checkbox1");
+  if (generalConditions.checked === false) {
+    console.log("Erreur vous devez accepter les conditions générales");
+  }
 }
