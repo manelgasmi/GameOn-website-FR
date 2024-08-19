6f728818ba1e130ba22e936fc6502832b8ace5f7
Manel Gasmi <manel.gasmi02@gmail.com>
2024-08-19 17:14:46 +0200

    issue #5 : corrections et amélioration du code

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,4 @@
+// applicate the responsive in the topnav
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -31,126 +32,144 @@ function closeModal() {
 // form validation
 function validate(event) {
   event.preventDefault();
+  
 
   // First name validation
+  // Retrieving DOM Elements
   const firstNameInput = document.getElementById("first");
   const firstNameError = document.getElementById("error-name");
+  // the value entered in the first name input
   const firstName = firstNameInput.value;
-  let isError = false;
-
+  // variable declaration "false" by default (there is no error)
+  let formHasError = false;
+  // the first condition appears the error message
   if (firstName.length < 2) {
     firstNameError.innerHTML =
       "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
     firstNameInput.classList.add("error-message-input");
-    isError = true;
+    formHasError = true;
+    // the second condition does not appear the error message
   } else {
     firstNameError.innerHTML = "";
     firstNameInput.classList.remove("error-message-input");
-    isError = false;
   }
 
   // Last name validation
   const lastNameInput = document.getElementById("last");
   const lastNameError = document.getElementById("error-lastName");
   const lastName = lastNameInput.value;
+  // the first condition appears the error message
   if (lastName.length < 2) {
     lastNameError.innerHTML =
       "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
     lastNameInput.classList.add("error-message-input");
-    isError = isError && true;
+    formHasError = true;
+    // the second condition does not appear the error message
   } else {
     lastNameError.innerHTML = "";
     lastNameInput.classList.remove("error-message-input");
-    isError = isError && false;
   }
 
   // email validation
   const emailInput = document.getElementById("email");
   const emailError = document.getElementById("error-email");
   let email = emailInput.value;
+  // This is a regular expression to check if the email address respect this format
   let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+.[a-z0-9._-]+");
+  // the first condition does not appears the error message
   if (emailRegExp.test(email)) {
     emailError.innerHTML = "";
     emailInput.classList.remove("error-message-input");
-    isError = false;
+    // the second condition appears the error message
   } else {
     emailError.innerHTML = "Veuillez entrer un email valide.";
     emailInput.classList.add("error-message-input");
-    isError = true;
+    formHasError = true;
   }
-  // error message of birth date
+  // date of birth validation
   const birthdateInput = document.getElementById("birthdate");
   const birthdateError = document.getElementById("birth-error");
   const birthdate = birthdateInput.value;
+  // minimum age required
+  const minAge = 18;
+  // calculate age
+  const toDay = new Date();
+  const birthDate = new Date(birthdate);
+  let age = toDay.getFullYear() - birthDate.getFullYear();
+  const monthDifference = toDay.getMonth() - birthDate.getMonth();
+  const dayDifference = toDay.getDate() - birthDate.getDate();
 
+  // Adjust age if the birthdate hasn't occurred yet this year
+  if (monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)) {
+    age--;
+  }
+  // the first condition appears the error message
   if (!birthdate) {
     birthdateError.innerHTML = "Veuillez entrer votre date de naissance.";
     birthdateInput.classList.add("error-message-input");
-    isError = isError && true;
+    formHasError = true;
+    // second condition appears the error message
+  } else if (age < minAge || (age === minAge && monthDifference < 0)) {
+    birthdateError.innerHTML = "Vous devez avoir au moins 18 ans.";
+    birthdateInput.classList.add("error-message-input");
+    formHasError = true;
+    // third condition does not appear the error message (the birthdate is entered correctly)
   } else {
     birthdateError.innerHTML = "";
     birthdateInput.classList.remove("error-message-input");
-    isError = isError && false;
   }
 
   // number of Competition validation
-  const nbrCompetitions = document.getElementById("quantity");
+  const nbrCompetitionsInput = document.getElementById("quantity");
   const errorNbr = document.getElementById("error-nbr");
-  const nbr = nbrCompetitions.value;
+  const nbr = nbrCompetitionsInput.value;
+  // the first condition appears the error message
   if (nbr < 0 || nbr > 99 || !nbr) {
     errorNbr.innerHTML =
       "Le nombre de compétition doit être compris entre 0 et 99.";
-    nbrCompetitions.classList.add("error-message-input");
-    isError = true;
+    nbrCompetitionsInput.classList.add("error-message-input");
+    formHasError = true;
+    // the second condition does not appear the error message
   } else {
     errorNbr.innerHTML = "";
-    nbrCompetitions.classList.remove("error-message-input");
-    isError = false;
+    nbrCompetitionsInput.classList.remove("error-message-input");
   }
 
   // input radio validation
-  let radioInputs = document.querySelectorAll('input[name = "location"]');
-  const locationError = document.getElementById("location-error");
   const selectedLocation = document.querySelector(
     'input[name="location"]:checked'
   );
+  const locationError = document.getElementById("location-error");
+  // the first condition appears the error message
   if (!selectedLocation) {
     locationError.innerHTML = "Vous devez sélectionner un emplacement.";
-    isError = true;
+    formHasError = true;
+    // the second condition does not appear the error message
   } else {
     locationError.innerHTML = "";
-    isError = false;
-  }
-  let locationChecked = false;
-  for (let i = 0; i < radioInputs.length; i++) {
-    if (radioInputs[i].checked) {
-      locationChecked = true;
-      break;
-    }
-  }
-  if (locationChecked === false) {
   }
 
   // general conditions box validation
   const generalConditions = document.getElementById("checkbox1");
   const checkboxError = document.getElementById("checkbox-error");
+  // the first condition appears the error message
   if (!generalConditions.checked) {
     checkboxError.innerHTML =
       "Vous devez accepter les conditions d'utilisation.";
-    isError = true;
+    formHasError = true;
+    // the second condition does not appear the error message
   } else {
     checkboxError.innerHTML = "";
-    isError = false;
-  }
-  if (generalConditions.checked === false) {
   }
 
   // show the thank you message
-  if (!isError) {
+  if (!formHasError) {
+    // this condition checks if formHasError is false
     const thankYouMessage = document.getElementById("thankYouMessage");
     const form = document.querySelector("form");
-
+    // add the class "hidden" to apply its properties
     form.classList.add("hidden");
+    // remove the class "hidden" to delete its properties
     thankYouMessage.classList.remove("hidden");
   }
 }
